a1127806ce66480ce9f1effb8b020bb15d136bc7
Ganjar Hadiatna <mdl.ganjarhadiatna@gmail.com>
2025-11-23 13:38:32 +0700

    update ui heros section in home

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -73,13 +73,14 @@ const updateScrollButtons = () => {
     <FrontLayout>
         <!-- Hero Section -->
         <template #front-prepend>
-            <section class="border-b bg-[#004aad] bg-gradient-to-r to-[#cb6ce6] py-16">
+            <section class="border-b bg-[#004aad] bg-gradient-to-r to-[#cb6ce6] py-8">
                 <div class="container mx-auto px-4 text-center">
                     <h1 class="mb-4 text-4xl font-bold tracking-tight lg:text-5xl text-white">
                         The <span class="font-extrabold text-white">Code Heroes</span> Journey
                     </h1>
-                    <p class="mx-auto mb-8 max-w-2xl text-md lg:text-lg text-white">
-                        Discover tutorials, code snippets, and development insights shared by the community. Learn, create, and grow together.
+                    <p class="mx-auto mb-8 max-w-3xl text-md lg:text-lg text-white">
+                        Discover tutorials, code snippets, and development insights shared by the community.<br />
+                        Learn, create, and grow together.
                     </p>
                     <div class="flex items-center justify-center gap-4">
                         <Button
