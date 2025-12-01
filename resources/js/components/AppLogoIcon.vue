c5fb3a011341ad026b8f4ea90ede61fe46a60312
Ganjar Hadiatna <49514655+ganjardbc@users.noreply.github.com>
2025-12-01 17:54:49 +0000

    refactor: simplify logo source logic in AppLogoIcon component

--- resources/js/components/AppLogoIcon.vue
+++ resources/js/components/AppLogoIcon.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 import type { HTMLAttributes } from 'vue';
-import { computed } from 'vue';
-import { useAppearance } from '@/composables/useAppearance';
-import LogoWhite from '../../assets/logo-white.png';
 import LogoFill from '../../assets/logo-fill.png';
 
 defineOptions({
@@ -14,14 +11,11 @@ interface Props {
 }
 
 defineProps<Props>();
-
-const { appearance } = useAppearance();
-const isLightMode = computed(() => appearance.value === 'light');
 </script>
 
 <template>
     <img
-        :src="isLightMode ? LogoFill : LogoWhite"
+        :src="LogoFill"
         :class="className"
         alt="Coderium"
     />
